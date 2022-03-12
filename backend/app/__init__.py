e5d41a5c89cfedcbd231ca4343077cfa43b4b0bd
Prince Pansari <princep@iitbhilai.ac.in>
2022-03-12 19:33:23 +0530

    login api added

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,9 +4,18 @@ from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
+from app.auth.routes import auth_routes
+
 app = Flask(__name__)
 
 api = Api(app)
 cors = CORS(app)
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
+
+auth_routes(api)
