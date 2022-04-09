d7bc45302e84ed4e2d48facda274fc8ec434c942
siramNIkhil2000 <siram.kumar@needl.ai>
2022-04-09 18:44:33 +0530

    autcheckstate bug fix filters addition on mycases page

--- frontend/src/case/myCasesPageFilters.jsx
+++ frontend/src/case/myCasesPageFilters.jsx
@@ -3,30 +3,36 @@ import DateRangePicker from '@mui/lab/DateRangePicker';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import Box from '@mui/material/Box';
-import { Paper, Stack, Typography, Button } from '@mui/material';
-import { useState } from 'react';
+import { Paper, Stack, Typography, Button, Autocomplete, Chip } from '@mui/material';
+import { useEffect, useState } from 'react';
 
 export default function Filters(props) {
     const [updatedOn, setUpdatedOn] = useState([null, null]);
     const [openedOn, setOpenedOn] = useState([null, null]);
-    const [patientName, setPatientName] = useState("");
+    const [patientName, setPatientName] = useState(null);
     const [createdBy, setCreatedBy] = useState("");
-    const [problem, setProblem] = useState("");
+    const [problems, setProblems] = useState([]);
     const [caseId, setCaseId] = useState("");
+    const [problemOptions, setProblemOptions] = useState([]);
+
+    useEffect(() => {
+        setProblemOptions([...new Set(props.cases.map((caseObj) => caseObj.problem))]);
+    }, [props.cases])
 
     const resetFilters = () => {
         setUpdatedOn([null, null]);
         setOpenedOn([null, null]);
         setPatientName("");
         setCreatedBy("");
-        setProblem("");
+        setProblems([]);
         setCaseId("");
+        props.setFilteredCases(props.cases);
     }
 
     const filterCases = () => {
         const filteredCases = props.cases.filter((caseItem) => {
             if (updatedOn[0] && updatedOn[1]) {
-                if (new Date(caseItem.updated_on) < updatedOn[0] || new Date(caseItem.updated_on) > updatedOn[1]) {
+                if (new Date(caseItem.updated_at) < updatedOn[0] || new Date(caseItem.updated_at) > updatedOn[1]) {
                     return false;
                 }
             }
@@ -45,12 +51,13 @@ export default function Filters(props) {
                     return false;
                 }
             }
-            if (problem) {
-                if (caseItem.problem.toLowerCase().indexOf(problem.toLowerCase()) === -1) {
+            if (problems.length) {
+                if (!problems.includes(caseItem.problem)) {
                     return false;
                 }
             }
             if (caseId) {
+                console.log(caseItem.case_id, caseId);
                 if (caseItem.case_id !== caseId) {
                     return false;
                 }
@@ -72,13 +79,28 @@ export default function Filters(props) {
                             endText="end"
                             value={updatedOn}
                             onChange={(newValue) => {
+                                if (newValue[1]) {
+                                    newValue[1].setHours(23, 59, 59, 999);
+                                }
                                 setUpdatedOn(newValue);
                             }}
                             renderInput={(startProps, endProps) => (
                                 <>
-                                    <TextField {...startProps} />
+                                    <TextField {...startProps}
+                                        size="small"
+                                        variant='filled'
+                                        InputProps={{
+                                            disableUnderline: true,
+                                        }}
+                                    />
                                     <Box sx={{ mx: 2 }}> to </Box>
-                                    <TextField {...endProps} />
+                                    <TextField {...endProps}
+                                        size="small"
+                                        variant='filled'
+                                        InputProps={{
+                                            disableUnderline: true,
+                                        }}
+                                    />
                                 </>
                             )}
                         />
@@ -88,48 +110,109 @@ export default function Filters(props) {
                             endText="end"
                             value={openedOn}
                             onChange={(newValue) => {
+                                if (newValue[1]) {
+                                    newValue[1].setHours(23, 59, 59, 999);
+                                }
                                 setOpenedOn(newValue);
                             }}
                             renderInput={(startProps, endProps) => (
                                 <>
-                                    <TextField {...startProps} />
+                                    <TextField {...startProps}
+                                        size="small"
+                                        variant='filled'
+                                        InputProps={{
+                                            disableUnderline: true,
+                                        }}
+                                    />
                                     <Box sx={{ mx: 2 }}> to </Box>
-                                    <TextField {...endProps} />
+                                    <TextField {...endProps}
+                                        size="small"
+                                        variant='filled'
+                                        InputProps={{
+                                            disableUnderline: true,
+                                        }}
+                                    />
                                 </>
                             )}
-                        />
-                        <Typography variant="body1" >Patient Name</Typography>
-                        <TextField
-                            id="patient_name"
-                            label="patient name"
-                            value={patientName}
-                            onChange={(newValue) => { setPatientName(newValue) }}
-
-                        />
-                        <Typography variant="body1" >Created By</Typography>
-                        <TextField
-                            id="created_by"
-                            label="created by"
-                            value={createdBy}
-                            onChange={(newValue) => { setCreatedBy(newValue) }}
-
-                        />
-                        <Typography variant="body1" >Problem</Typography>
-                        <TextField
-                            id="problem"
-                            label="problem"
-                            value={problem}
-                            onChange={(newValue) => { setProblem(newValue) }}
-
                         />
                         <Typography variant="body1" >Case Id</Typography>
                         <TextField
                             id="case_id"
                             label="case id"
-                            type="number"
                             value={caseId}
-                            onChange={(newValue) => { setCaseId(newValue) }}
+                            sx={{ width: 230 }}
+                            onChange={(e) => {
+                                if (e.target.value !== "")
+                                    setCaseId(Number(e.target.value));
+                                else
+                                    setCaseId(e.target.value);
 
+                            }}
+                            size="small"
+                            variant='filled'
+                            InputProps={{
+                                disableUnderline: true,
+                            }}
+                        />
+                        <Typography variant="body1" >Patient Name</Typography>
+                        <Autocomplete
+                            size="small"
+                            id="patient_name"
+                            // value={patientName}
+                            onChange={(event, newValue) => {
+                                setPatientName(newValue);
+                                console.log(newValue);
+                            }}
+                            options={[...new Set(props.cases.map((caseObj) => caseObj.patient_name))]}
+                            sx={{ width: 230 }}
+                            renderInput={(params) => <TextField
+                                variant='filled'
+                                {...params}
+                                label="Patient Name"
+                            />}
+                        />
+                        <Typography variant="body1" >Created By</Typography>
+                        <Autocomplete
+                            size="small"
+                            id="created_by"
+                            // value={createdBy}
+                            onChange={(event, newValue) => {
+                                setCreatedBy(newValue);
+                                console.log(newValue);
+                            }}
+                            options={[...new Set(props.cases.map((caseObj) => caseObj.created_by))]}
+                            sx={{ width: 230 }}
+                            renderInput={(params) => <TextField
+                                variant='filled'
+                                {...params}
+                                label="Created By"
+                            />}
+                        />
+
+                        <Typography variant="body1" >Problem</Typography>
+                        <Autocomplete
+                            multiple
+                            sx={{ width: 510 }}
+                            // value={problems}
+                            onChange={(event, newValue) => {
+                                setProblems(newValue);
+                            }}
+                            id="problem"
+                            options={problemOptions}
+                            renderTags={(value, getTagProps) =>
+                                value.map((option, index) => (
+                                    <Chip key={option + index} variant="outlined" label={option} {...getTagProps({ index })} />
+                                ))
+                            }
+                            renderInput={(params) => (
+                                <TextField
+                                    {...params}
+                                    variant="filled"
+                                    label="problem"
+                                    placeholder="Select Problems"
+                                    size="small"
+                                />
+                            )}
                         />
                     </LocalizationProvider>
                     <Stack direction='row' spacing={2}>
