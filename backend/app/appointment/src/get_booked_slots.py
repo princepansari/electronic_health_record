4c2cf98f57b0501d51b8754d98e09328ad6a85d4
ryker818 <asjaiswal666@gmail.com>
2022-04-18 20:48:05 +0530

    Booked slots

--- backend/app/appointment/src/get_booked_slots.py
+++ backend/app/appointment/src/get_booked_slots.py
@@ -3,7 +3,8 @@ from flask_restful import Resource
 from http import HTTPStatus
 import bleach
 import datetime
-
+from schema import Schema, And, Use
+from flask import request
 import os
 import sys
 
@@ -17,9 +18,10 @@ class GetBookedSlots(Resource):
     def __init__(self):
         self.rds = RDS()
         self.schema = Schema({
-            'doctor_id': And(uuid, Use(bleach.clean), Utils.validate_id)
+            'doctor_id': And(str, Use(bleach.clean)),
+            from_date : And(str, Use(Utils.convert_to_datetime))
         })
-
+    @jwt_required()
     def get(self):
         if not self.schema.is_valid(request.get_json()):
             return {'message': 'Invalid data'}, HTTPStatus.BAD_REQUEST
@@ -27,10 +29,11 @@ class GetBookedSlots(Resource):
         data = self.schema.validate(request.get_json())
         doctor_id = data['doctor_id']
         from_date = data['from_date']
+        last_date= from_date+datetime.timedelta(days=8)
 
-        @jwt_required()
-        slot = self.rds.get_booked_slots(doctor_id=doctor_id, from_date=from_date)
+        
+        slot = self.rds.get_booked_slots(doctor_id=doctor_id, from_date=from_date, last_date=last_date)
 
         if not slot:
-            return {'message': 'You are not a doctor'}, HTTPStatus.NOT_FOUND
+            return {'message': 'No slot'}, HTTPStatus.NOT_FOUND
         return slot, HTTPStatus.OK
