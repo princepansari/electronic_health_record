a51f0734be892a460ac4f2984f5a40dd5cd0d6c8
siramNIkhil2000 <siram.kumar@needl.ai>
2022-04-20 15:03:28 +0530

    some ui fixes in upcoming appointments fix in create case form fix in appointment table fix in ui of filters in my case page

--- frontend/src/UpcomingAppointmentsPage/UpcomingAppointmentsPage.jsx
+++ frontend/src/UpcomingAppointmentsPage/UpcomingAppointmentsPage.jsx
@@ -3,21 +3,21 @@ import React from 'react';
 import AppointmentItem from './AppointmentItem';
 
 const appointments = [
-    {case: 1, patientName: 'Yo Yo Boy', doctorName: 'Dr. M.J. Memon', startTime: Date.now(), endTime: Date.now(), date: Date.now() },
-    {case: 2, patientName: 'Yo Yo Boy', doctorName: 'Dr. M.J. Memon', startTime: Date.now(), endTime: Date.now(), date: Date.now() },
-    {case: 3, patientName: 'Yo Yo Boy', doctorName: 'Dr. M.J. Memon', startTime: Date.now(), endTime: Date.now(), date: Date.now() },
-    {case: 4, patientName: 'Yo Yo Boy', doctorName: 'Dr. M.J. Memon', startTime: Date.now(), endTime: Date.now(), date: Date.now() },
-    {case: 5, patientName: 'Yo Yo Boy', doctorName: 'Dr. M.J. Memon', startTime: Date.now(), endTime: Date.now(), date: Date.now() },
+  { case: 1, patientName: 'Yo Yo Boy', doctorName: 'Dr. M.J. Memon', startTime: Date.now(), endTime: Date.now(), date: Date.now() },
+  { case: 2, patientName: 'Yo Yo Boy', doctorName: 'Dr. M.J. Memon', startTime: Date.now(), endTime: Date.now(), date: Date.now() },
+  { case: 3, patientName: 'Yo Yo Boy', doctorName: 'Dr. M.J. Memon', startTime: Date.now(), endTime: Date.now(), date: Date.now() },
+  { case: 4, patientName: 'Yo Yo Boy', doctorName: 'Dr. M.J. Memon', startTime: Date.now(), endTime: Date.now(), date: Date.now() },
+  { case: 5, patientName: 'Yo Yo Boy', doctorName: 'Dr. M.J. Memon', startTime: Date.now(), endTime: Date.now(), date: Date.now(), type: "new", patientEmail: "abc@gmail.com" },
 ];
 
 const UpcomingAppointmentsPage = () => {
   return (
     <div>
-      <Grid 
+      <Grid
         container
         spacing={2}
         sx={{
-          mt: {xs: 8, md: 10},
+          mt: { xs: 8, md: 10 },
           display: 'flex',
           flexDirection: 'column',
           justifyContent: 'center',
@@ -26,17 +26,7 @@ const UpcomingAppointmentsPage = () => {
       >
         {
           appointments.map(appointment => (
-            <Grid item 
-            key={appointment.case}
-            sx={{ 
-              width: {xs: '90%', md: '65%'},
-              height: '130px',
-              borderRadius: '10px',
-              backgroundColor: '#d9dad7',
-              my: 1
-            }}>
-              <AppointmentItem appointment={appointment}/>
-            </Grid>
+            <AppointmentItem key={appointment} appointment={appointment} />
           ))
         }
       </Grid>
