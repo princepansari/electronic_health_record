4ad79f1ef7f3a085dce0c4ece44d2e9945863b59
Prince Pansari <princep@iitbhilai.ac.in>
2022-04-28 23:32:05 +0530

    mail issue for create appointment resolved

--- backend/app/appointment/src/create_appointment.py
+++ backend/app/appointment/src/create_appointment.py
@@ -37,7 +37,7 @@ class CreateAppointment(Resource):
         data = self.schema.validate(request.get_json())
         doctor_id = data['doctor_id']
         patient_id = get_jwt_identity()
-        email  = get_jwt()['email']
+        email = get_jwt()['email']
         name = get_jwt()['name']
 
         appointment_datetime = data['appointment_datetime']
@@ -50,7 +50,7 @@ class CreateAppointment(Resource):
         appointment_date = appointment_datetime.date()
         day_name = calendar.day_name[appointment_date.weekday()].lower()
         appointment_time = appointment_datetime.time()
-        ist_appointment_time = appointment_time.astimezone(timezone('Asia/Kolkata'))
+        ist_appointment_time = appointment_datetime.astimezone(timezone('Asia/Kolkata'))
         doctor_availability = self.rds.get_doctors_schedule_by_id(doctor_id=doctor_id)
         slot_availability = self.rds.get_slot_availability(appointment_time=appointment_datetime)
         if doctor_availability['days'][day_name]:
@@ -61,10 +61,13 @@ class CreateAppointment(Resource):
                     appointment_id = self.rds.create_appointment(doctor_id=doctor_id, patient_id=patient_id,
                                                                  appointment_datetime=appointment_datetime,
                                                                  followup_prescription_id=followup_prescription_id)
-                    doctor_name = self.rds.get_doctor_name(doctor_id=doctor_id)
+                    doctor_name = self.rds.get_doctor_name(doctor_id=doctor_id)['name']
+                    message = f'Hello {name},\n\nYour appointment with Dr. {doctor_name} has been confirmed.\n\n' + \
+                              f'Appointment Date: {ist_appointment_time.date().isoformat()}\n' + \
+                              f'Appointment Time: {ist_appointment_time.strftime("%H:%M %p")}\n\n'
                     CreateAppointment.send_email(to=[email],
-                                 subject='Appointment Booked',
-                                 message=f'Dear {name},\nYour appointment booked with {doctor_name}\nAppointment ID is {appointment_id}.\nAppointment time is {ist_appointment_time.strftime(format)}')
+                                                 subject='Appointment Booked',
+                                                 message=message)
                     return appointment_id, HTTPStatus.OK
                 else:
                     return {'message': 'slot is booked'}, HTTPStatus.BAD_REQUEST
@@ -72,8 +75,6 @@ class CreateAppointment(Resource):
                 return {'message': 'Doctor is not available'}, HTTPStatus.BAD_REQUEST
         else:
             return {'message': 'Doctor is not available'}, HTTPStatus.BAD_REQUEST
-
-
 
     @staticmethod
     def send_email(*, to, subject, message):
